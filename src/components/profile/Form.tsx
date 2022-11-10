9ef22e816ed4ec65b6cca008ce2ebbe3b0dcb0b3
AlexB <a.basalov@a3f.ru>
2022-11-10 23:13:49 +0400

    forms

--- src/components/profile/Form.tsx
+++ src/components/profile/Form.tsx
@@ -1,17 +1,60 @@
 import React from "react"
 import Button from "../button/Button";
+import "./style.css";
+import tz from '../../const/tz.json'
+import { spawn } from "child_process";
+
+
+// const initialState = {
+//     name: '',
+//     timezone: '',
+//     language: '',
+//     name: '',
+// };
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'increment':
+//       return {count: state.count + 1};
+//     case 'decrement':
+//       return {count: state.count - 1};
+//     default:
+//       throw new Error();
+//   }
+// }
 
 interface IProps { }
 
 const Form: React.FC<IProps> = (props) => {
+
+    const options = ['Russian', 'English']
+    const timezones = tz.map((el) => {
+        return el.utc.map(zone => {
+            const utc = el.text.split(' ')[0];
+            return (
+                <option className="option" value={zone}>
+                    {utc}&nbsp;{zone}
+                </option>
+            )
+        })
+
+
+
+    })
+
     return (
-        <form>
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
-        <Button title="отправить"/>
-    </form >
-  )
+        <form className="form">
+            <input className="input" type="text" />
+            <input className="input" type="text" />
+            <select className="select">
+                {options.map((el, index) => <option value={index}>{el}</option>)}
+            </select>
+            <select className="select">
+                {timezones}
+            </select>
+            <Button title="отправить" />
+        </form >
+    )
 };
 
 
--- src/components/weather/Form.tsx
+++ src/components/weather/Form.tsx
@@ -1,17 +1,45 @@
 import React from "react"
 import Button from "../button/Button";
+import "./style.css"
+
+
+// const initialState = {
+//     city: '',
+//     timezone: '',
+
+// };
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'increment':
+//       return {count: state.count + 1};
+//     case 'decrement':
+//       return {count: state.count - 1};
+//     default:
+//       throw new Error();
+//   }
+// }
 
 interface IProps { }
 
 const Form: React.FC<IProps> = (props) => {
+
+    // const [state, dispatch] = React.useReducer(reducer)
+    const options = ['Russian', 'English']
+
     return (
-        <form>
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
-        <Button title="отправить"/>
-    </form >
-  )
+        <form className="form">
+            <input className="input" type="text" />
+            <input className="input" type="text" />
+            <select className="select">
+                {options.map((el, index) => <option value={index}>{el}</option>)}
+            </select>
+            <select className="select">
+                {options.map((el, index) => <option value={index}>{el}</option>)}
+            </select>
+            <Button title="отправить" />
+        </form >
+    )
 };
 
 
