c9d5e2b1e327a8f74375e6b020a8b063dd21eaef
AlexB <a.basalov@a3f.ru>
2022-12-30 19:00:34 +0400

    prod route fix

--- src/const/routes.tsx
+++ src/const/routes.tsx
@@ -5,7 +5,7 @@ import { EVENT_TYPE } from "./event";
 import { createHashRouter } from "react-router-dom";
 
 // export const BASE_ROUTE = 'http://localhost:5000/api/'; //local
-export const BASE_ROUTE = 'http://188.166.56.12:5000'; //prod
+export const BASE_ROUTE = 'http://188.166.56.12:5000/api/'; //prod
 
 export const PREFIX = 'tg-bot-frontend-v1';
 
